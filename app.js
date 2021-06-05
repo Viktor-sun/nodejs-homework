c768ede0b24eab6ab32a6ef6918adb51fe6b337d
Viktor-sun <bobick.zdoh@gmail.com>
2021-06-05 13:33:32 +0300

    add helmet

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const app = express()
 const logger = require('morgan')
 const cors = require('cors')
 const { HttpCode } = require('./helpers/constants')
+const helmet = require('helmet')
 
 const contactsRouter = require('./routes/api/api-contacts')
 const usersRouter = require('./routes/api/api-users')
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
+app.use(helmet())
 app.use(logger(formatsLogger))
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: 10000 }))
 
 app.use('/api/contacts', contactsRouter)
 app.use('/api/users', usersRouter)
